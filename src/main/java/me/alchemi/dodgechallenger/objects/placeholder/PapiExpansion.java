bbfc6cf9b1458d974e7954e04349abbc41138049
Alchemi <misha.smit@xs4all.nl>
2019-10-09 11:43:03 +0200

    Catch errors for no island in papiexpansion

--- src/main/java/me/alchemi/dodgechallenger/objects/placeholder/PapiExpansion.java
+++ src/main/java/me/alchemi/dodgechallenger/objects/placeholder/PapiExpansion.java
@@ -38,38 +38,55 @@ public class PapiExpansion extends PlaceholderExpansion{
 	public String onPlaceholderRequest(Player p, String id) {
 
 		if (id.equals("rank")) {
-			DodgeIsland island = getIslandManagerFromPlayer(p);
-			if (island == null) return "0";
-			return String.valueOf(island.getRank().getId());
+			try {
+				DodgeIsland island = getIslandManagerFromPlayer(p);
+				return String.valueOf(island.getRank().getId()); 
+			} catch (IllegalAccessError e) {
+				return "0";
+			}
 			
 		} else if (id.equals("stringed_rank")) {
-			DodgeIsland island = getIslandManagerFromPlayer(p);
-			if (island == null) return Messenger.formatString(RankManager.getManager().getFirst().getDisplayName());
-			return Messenger.formatString(island.getRank().getDisplayName());
+			try {
+				DodgeIsland island = getIslandManagerFromPlayer(p);
+				return Messenger.formatString(island.getRank().getDisplayName());
+			} catch(IllegalAccessError e) {
+				return Messenger.formatString(RankManager.getManager().getFirst().getDisplayName());
+			}
 			
 		} else if (id.equals("stringed_rank_no_format")) {
-			DodgeIsland island = getIslandManagerFromPlayer(p);
-			if (island == null) return ChatColor.stripColor(RankManager.getManager().getFirst().getDisplayName());
-			return ChatColor.stripColor(island.getRank().getDisplayName());
+			try {
+				DodgeIsland island = getIslandManagerFromPlayer(p);
+				return ChatColor.stripColor(island.getRank().getDisplayName());
+			} catch (IllegalAccessError e) {
+				return ChatColor.stripColor(RankManager.getManager().getFirst().getDisplayName());
+			}
 			
 		} else if (id.equals("prefix")) {
-			DodgeIsland island = getIslandManagerFromPlayer(p);
-			if (island == null) return RankManager.getManager().getFirst().getPrefix().trim();
-			return island.getRank().getPrefix().trim();
+			try {
+				DodgeIsland island = getIslandManagerFromPlayer(p);
+				return island.getRank().getPrefix().trim();
+			} catch (IllegalAccessError e) {
+				return RankManager.getManager().getFirst().getPrefix().trim();
+			}
 			
 		} else if (id.equals("amount_challenges_completed")) {
-			DodgeIsland island = getIslandManagerFromPlayer(p);
-			if (island == null) return "0";
-			
-			return String.valueOf(island.getChallenges().size());
+			try{
+				DodgeIsland island = getIslandManagerFromPlayer(p);
+				return String.valueOf(island.getChallenges().size());
+			} catch (IllegalAccessError e) {
+				return "0";
+			}
 			
 		} else if (id.matches("(challenge_completed_)\\w+")) {
 			String c = id.replace("challenge_completed_", "");
 			if (Challenge.getChallengeFromID(c) == null) return "false";
 			
-			DodgeIsland island = getIslandManagerFromPlayer(p);
-			if (island == null) return "false";
-			return String.valueOf(island.getChallenges().contains(Challenge.getChallengeFromID(c)));
+			try {
+				DodgeIsland island = getIslandManagerFromPlayer(p);
+				return String.valueOf(island.getChallenges().contains(Challenge.getChallengeFromID(c)));
+			} catch (IllegalAccessError e) {
+				return "false";
+			}
 			
 		}
 		
